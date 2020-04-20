1ce091a8d8a99820ef6f3127c6bf565aaf5b828f
Roy Ivy III <rivy.dev@gmail.com>
2020-04-19 23:43:59 -0500

    refactor/polish ~ fix CMD lint warnings (function exports and comments)

--- cmd/git-changelog/main_test.go
+++ cmd/git-changelog/main_test.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
-    "github.com/urfave/cli"
+	"github.com/stretchr/testify/assert"
+	"github.com/urfave/cli"
 )
 
 var gAssert *assert.Assertions
 
 func mockAppAction(c *cli.Context) error {
-    assert := gAssert
-    assert.Equal("c.yml", c.String("config"))
-    assert.Equal("^v", c.String("tag-filter-pattern"))
-    assert.Equal("o.md", c.String("output"))
-    assert.Equal("v5", c.String("next-tag"))
-    assert.True(c.Bool("silent"))
-    assert.True(c.Bool("no-color"))
-    assert.True(c.Bool("no-emoji"))
-    return nil
+	assert := gAssert
+	assert.Equal("c.yml", c.String("config"))
+	assert.Equal("^v", c.String("tag-filter-pattern"))
+	assert.Equal("o.md", c.String("output"))
+	assert.Equal("v5", c.String("next-tag"))
+	assert.True(c.Bool("silent"))
+	assert.True(c.Bool("no-color"))
+	assert.True(c.Bool("no-emoji"))
+	return nil
 }
 
 func TestCreateApp(t *testing.T) {
-    assert := assert.New(t)
-    assert.True(true)
-    gAssert = assert
+	assert := assert.New(t)
+	assert.True(true)
+	gAssert = assert
 
-    app := CreateApp(mockAppAction)
-    args := []string{
-        "git-changelog",
-        "--silent",
-        "--no-color",
-        "--no-emoji",
-        "--config", "c.yml",
-        "--output", "o.md",
-        "--next-tag", "v5",
-        "--tag-filter-pattern", "^v",
-    }
-    app.Run(args)
+	app := createApp(mockAppAction)
+	args := []string{
+		"git-changelog",
+		"--silent",
+		"--no-color",
+		"--no-emoji",
+		"--config", "c.yml",
+		"--output", "o.md",
+		"--next-tag", "v5",
+		"--tag-filter-pattern", "^v",
+	}
+	app.Run(args)
 }
